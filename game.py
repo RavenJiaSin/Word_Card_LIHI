1b2368d2ab6eaebe4348657cf520f2dc0ea66133
Wood <jhuangchenleo@gmail.com>
2025-04-19 00:42:30 +0800

    init: main_state, start_state

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 import pygame as pg
-from modules.state import State, Menu_State
+from modules.state import State, Menu_State, Start_State
 from modules.manager import font_map
 
 FPS = 60
@@ -13,12 +13,12 @@ event_list = None
 def chage_state(state:State):
     pg.event.post(pg.event.Event(EVENT_CHANGE_STATE, {"state":state}))
 
-def draw_text(surf, text, size, x, y):
-    font = pg.font.Font(font_map['test_font'], size)
+def draw_text(surf, text, size, x, y, font='SWEISANSCJKTC-REGULAR'):
+    font = pg.font.Font(font_map[font], size)
     text_surface = font.render(text, True, (200, 200, 200))
     text_rect = text_surface.get_rect()
     text_rect.centerx = x
-    text_rect.top = y
+    text_rect.centery = y
     surf.blit(text_surface, text_rect)
 
 class Game:
@@ -26,7 +26,7 @@ class Game:
         pg.init()
         self.__isRunning = True
         self.__clock = pg.time.Clock()
-        self.__state = Menu_State()
+        self.__state = Start_State()
         global window
         window = pg.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
     def run(self):
--- modules/state/__init__.py
+++ modules/state/__init__.py
@@ -1,3 +1,5 @@
 from .state import State
 from .test_state import Test_State
 from .menu_state import Menu_State
+from .start_state import Start_State
+from .main_state import Main_State
