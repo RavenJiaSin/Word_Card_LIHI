60df83483378e66ba09d7ed49d25c4b77f09d19e
YH325 <eric0325.tai@gmail.com>
2025-04-20 16:53:35 +0800

    fix: 新增關閉放大卡片的功能

--- modules/state/card_collection_state.py
+++ modules/state/card_collection_state.py
@@ -4,6 +4,7 @@ from .state import State
 from ..object import Button
 from ..object import Text_Button
 from ..object import Card
+from functools import partial
 
 class Card_Collection_State(State):
 
@@ -23,12 +24,42 @@ class Card_Collection_State(State):
         for row in range(2):
             for col in range(5):
                 card = Card(pos=(360 + col * 300, 360 + row * 300), size=150, name='test_poker')
-                card.setClick(lambda c=card: self.enlarge_card(c))
+                card.setClick(partial(self.try_enlarge_card, card))
                 self.background_cards.add(card)
 
     def enlarge_card(self, card):
         self.foreground_card = Card(pos=(game.CANVAS_WIDTH/2, game.CANVAS_HEIGHT/2), size=300, name='test_poker')
         self.foreground_card_group.add(self.foreground_card)
+
+    def try_enlarge_card(self, card):
+        if self.foreground_card:
+            self.foreground_card_group.empty()
+            self.foreground_card = None
+        else:
+            self.enlarge_card(card)
+
+    def handle_event(self, event):
+        if event.type == pg.MOUSEBUTTONDOWN:
+            
+            mouse_pos = pg.mouse.get_pos()
+
+            if self.foreground_card:
+                # 如果點的位置不在卡片上，就關掉
+                if not self.foreground_card.rect.collidepoint(event.pos):
+                    self.foreground_card_group.empty()
+                    self.foreground_card = None
+
+            else:
+            # 沒有放大卡 → 檢查是否點到背景卡牌或 UI 按鈕
+                for sprite in self.ui_sprites:
+                    if sprite.rect.collidepoint(mouse_pos) and hasattr(sprite, 'onClick'):
+                        sprite.onClick()
+                        return
+
+                for card in self.background_cards:
+                    if card.rect.collidepoint(mouse_pos) and hasattr(card, 'onClick'):
+                        card.onClick()
+                        return
 
     # override
     def update(self):
@@ -42,7 +73,7 @@ class Card_Collection_State(State):
         game.draw_text(game.canvas, "Card Collection", 70, game.CANVAS_WIDTH/2 + 50 , 100)
         self.background_cards.draw(game.canvas)  # 背景卡牌
         if self.foreground_card:
-            dark_overlay = pg.Surface((game.CANVAS_WIDTH, game.CANVAS_HEIGHT), flags=pg.SRCALPHA)
+            dark_overlay = pg.Surface((game.CANVAS_WIDTH, game.CANVAS_HEIGHT), flags=pg.SRCALPHA) #黑幕頁面，製造聚焦效果
             dark_overlay.fill((0, 0, 0, 180))  # RGBA，最後一個值是透明度（0~255）
             game.canvas.blit(dark_overlay, (0, 0))  # 把暗幕畫上去
             self.foreground_card_group.draw(game.canvas)
