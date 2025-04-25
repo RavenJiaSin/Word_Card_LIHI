2501490110b48f2c41e75f8e38ccee2be4ca147b
Toby <penguinoftoby@gmail.com>
2025-04-26 00:40:39 +0800

    feat: 連連看的卡片配對成功會有秩序的放置  英文在上，中文在下

--- modules/object/match_game.py
+++ modules/object/match_game.py
@@ -10,25 +10,39 @@ class Match_Game:
     """配對遊戲物件。
 
     管理配對小遊戲的物件
+
+    Attributes:
+        __first_chosen_card (Card): 第一個被選取的卡片
+        __second_chosen_card (Card): 第二個被選取的卡片
+        __pending_wrong_time (int):  配對錯誤而將卡片翻回去背面的tick時間(預設等待 800 ms)
+        __pending_right_time (int):  配對成功而將卡片移動的tick時間(預設等待 800 ms)
+        __blue_turn (bool): 是否為藍方回合
+        __blue_score (int): 藍方分數
+        __red_score(int): 紅方分數
+        __card_sprites (pg.sprite.LayeredUpdates): 所有卡片群組
+        __paired_cards (pg.sprite.LayeredUpdates): 配對成功卡片群組
+        __layer (int): 繪畫圖層, 當新的配對出現則加一, 使其顯示在最上層
     """
     __first_chosen_card = None
     __second_chosen_card = None
-    __pending_flip_time = None
+    __pending_wrong_time = None
+    __pending_right_time = None
     __blue_turn = True
     __blue_score = 0
     __red_score = 0
-
+    __all_cards = pg.sprite.LayeredUpdates()
+    __paired_cards = pg.sprite.LayeredUpdates()
+    __layer = 0
     def __init__(self, cols:int=4, rows:int=4, top_left:tuple=(530,270), col_spacing:int=280, row_spacing:int=230):
         grid = [[(top_left[0] + c * col_spacing, top_left[1] + r * row_spacing) for c in range(cols)] for r in range(rows)]
-        self.__card_sprites = pg.sprite.Group()
 
-        words = self.__getWords(cols*rows // 2)
+        words = self.__getWords((cols*rows / 2).__ceil__())
         i = 0
         for row in grid:
             for pos in row:
                 card = Match_Card(pos=pos, scale=1.6, word=words[i], font_size=24)
                 i += 1
-                self.__card_sprites.add(card)
+                self.__all_cards.add(card, layer=0)
 
         game.background_color = (50,50,100)
 
@@ -47,61 +61,85 @@ class Match_Game:
         current_time = pg.time.get_ticks()
 
         # 卡片配對錯誤，等待800ms後，翻到卡背
-        if self.__pending_flip_time is not None:
-            if current_time >= self.__pending_flip_time:
+        if self.__pending_wrong_time is not None:
+            if current_time >= self.__pending_wrong_time:
+                self.__pending_wrong_time = None
                 self.__first_chosen_card.can_press = True
                 self.__second_chosen_card.can_press = True
                 self.__first_chosen_card.flip()
                 self.__second_chosen_card.flip()
                 self.__first_chosen_card = None
                 self.__second_chosen_card = None
-                self.__pending_flip_time = None
                 self.__set_all_card_flip(True)
                 self.__change_player_turn()
             return
-
-        # 選擇兩張卡片翻開後，如果配對成功，則放入__correct_cards中鎖定(不能再次翻開)
-        if self.__first_chosen_card and self.__second_chosen_card:
-            first_word = self.__first_chosen_card.get_word()
-            second_word = self.__second_chosen_card.get_word()
-            if (self.__ans.get(first_word) != second_word) and (self.__ans.get(second_word) != first_word):
-                self.__pending_flip_time = current_time + 800
-                self.__set_all_card_flip(False)
-            else:
+        
+        # 卡片配對成功，等待800ms後，移動至旁邊並對玩家加分
+        if self.__pending_right_time is not None:
+            if current_time >= self.__pending_right_time:
+                self.__pending_right_time = None
+                self.__paired_cards.add(self.__first_chosen_card)
+                self.__paired_cards.add(self.__second_chosen_card)
+                self.__layer += 1
+                self.__all_cards.change_layer(self.__first_chosen_card, self.__layer)
+                self.__all_cards.change_layer(self.__second_chosen_card, self.__layer)
                 self.__first_chosen_card.setWiggle()
                 self.__second_chosen_card.setWiggle()
                 if self.__blue_turn:
                     self.__blue_score += 1
-                    self.__first_chosen_card.moveTo((150, 300), 1)
-                    self.__second_chosen_card.moveTo((150, 300), 1)
+                    if(self.__first_chosen_card.get_word()[1] == 'eng'):
+                        self.__first_chosen_card.moveTo((150, 300), 1)
+                        self.__second_chosen_card.moveTo((150, 500), 1)
+                    else:
+                        self.__second_chosen_card.moveTo((150, 300), 1)
+                        self.__first_chosen_card.moveTo((150, 500), 1)
                 else:
                     self.__red_score += 1
-                    self.__first_chosen_card.moveTo((1750, 300), 1)
-                    self.__second_chosen_card.moveTo((1750, 300), 1)
+                    if(self.__first_chosen_card.get_word()[1] == 'eng'):
+                        self.__first_chosen_card.moveTo((1750, 300), 1)
+                        self.__second_chosen_card.moveTo((1750, 500), 1)
+                    else:
+                        self.__second_chosen_card.moveTo((1750, 300), 1)
+                        self.__first_chosen_card.moveTo((1750, 500), 1)
                 self.__first_chosen_card = None
                 self.__second_chosen_card = None
+                self.__set_all_card_flip(True)
+            return
 
-    def getScore(self) -> tuple[0,0]:
+        # 選擇兩張卡片翻開後，如果配對成功，則放入__correct_cards中鎖定(不能再次翻開)
+        if self.__first_chosen_card and self.__second_chosen_card:
+            first_word = self.__first_chosen_card.get_word()[0]
+            second_word = self.__second_chosen_card.get_word()[0]
+            if (self.__ans.get(first_word) == second_word) or (self.__ans.get(second_word) == first_word):
+                self.__pending_right_time = current_time + 800
+                self.__set_all_card_flip(False)
+            else:
+                self.__pending_wrong_time = current_time + 800
+                self.__set_all_card_flip(False)
+                
+
+    def getScore(self) -> tuple:
         return (self.__blue_score, self.__red_score)
 
-    def getGroup(self) -> pg.sprite.Group:
-        return self.__card_sprites
+    def getSpriteGroup(self) -> pg.sprite.LayeredUpdates:
+        return self.__all_cards
     
     def __getWords(self, n) -> list:
         db = VocabularyDB()
         words = db.find_vocabulary(column='Vocabulary', length=random.randrange(5,8))
-        random_words = [word[0] for word in random.sample(words, n)]
-        random_words_trans = [db.find_vocabulary(column='Translation', voc=word)[0][0] for word in random_words]
-        random_words_trans = [chinese.split(';')[0].split(',')[0] for chinese in random_words_trans]
-        self.__ans = dict(zip(random_words, random_words_trans))
+        random_words = [(word['Vocabulary'], 'eng') for word in random.sample(words, n)]
+        random_words_trans = [db.find_vocabulary(voc=word[0])[0]['Translation'] for word in random_words]
+        random_words_trans = [(chinese.split(';')[0].split(',')[0], 'chi') for chinese in random_words_trans]
+        self.__ans = {eng[0]: chi[0] for eng, chi in zip(random_words, random_words_trans)}
 
         mixed_words = random_words + random_words_trans
         random.shuffle(mixed_words)
         return mixed_words
     
     def __set_all_card_flip(self, can_flip):
-        for card in self.__card_sprites:
-            card.can_press = can_flip
+        for card in self.__all_cards:
+            if card not in self.__paired_cards:
+                card.can_press = can_flip
 
     def __change_player_turn(self):
         self.__blue_turn = not self.__blue_turn
