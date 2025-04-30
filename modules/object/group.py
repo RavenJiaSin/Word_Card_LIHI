55668c38aaad2cf046322575014f1ea124cf4abf
Toby <penguinoftoby@gmail.com>
2025-04-30 13:19:19 +0800

    chore: 新增Group類別註解

--- modules/object/group.py
+++ modules/object/group.py
@@ -1,6 +1,15 @@
 import pygame as pg
 
 class Group(pg.sprite.LayeredUpdates):
+    """群組物件。繼承自`pg.sprite.LayeredUpdates`。
+    
+    為了新增handle_event而新增的類別
+
+    其餘操作皆和pg.sprite.Group相同
+
+    LayeredUpdates又多了可以操作圖層的功能
+
+    """
     def __init__(self, *sprites, **kwargs):
         super().__init__(*sprites, **kwargs)
 
