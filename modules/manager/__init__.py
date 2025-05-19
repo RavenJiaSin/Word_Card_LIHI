54620e2f751aa4f833f8e3ffaa115de9904df150
Wood <jhuangchenleo@gmail.com>
2025-05-19 16:02:43 +0800

    feat: 隨時間減少耐久值

--- modules/manager/__init__.py
+++ modules/manager/__init__.py
@@ -2,4 +2,5 @@ from .font_manager import Font_Manager
 from .image_manager import Image_Manager
 from .event_manager import Event_Manager
 from .train_mode import Train_Enum
+from .card_manager import Card_Manager
 from .time_manager import Time_Manager
