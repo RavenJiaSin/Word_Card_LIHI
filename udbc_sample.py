34e28c93ac725ab48caa7f3ee8a08ffecd90e5af
RavenJiaSin <99117abraham@gmail.com>
2025-05-19 14:53:54 +0800

    feat: userDBconnect.py 新增update_durability() 用來一次更新(+-delta)指定熟練度卡牌的耐久度(0~100)

--- udbc_sample.py
+++ udbc_sample.py
@@ -12,13 +12,16 @@ user_db = UserDB()
 # user_db.update_user_info(user_id = 1, level = 1)
 
 # 為user新增一卡牌
-# user_db.add_card_to_user(user_id = 1, voc_id='2222_able')
+# user_db.add_card_to_user(user_id = 1, voc_id='2_adult')
 
 # 查詢使用者有的卡牌資訊
 # print(user_db.get_card_info(user_id = 1, voc_id = '0_able'))
 
 # 修改user的卡牌資料
-user_db.update_card_info(user_id = 1, voc_id = '0_able', durability = 90)
+# user_db.update_card_info(user_id = 1, voc_id = '0_able', durability = 90)
+user_db.update_card_info(user_id = 1, voc_id = '0_able', proficiency = 2)
 
 # 查詢 card_collection 表中耐久度(durability) "<= N" 的卡牌
 # print(user_db.get_card_durability_below(user_id = 1, durability = 100))
+
+user_db.update_durability(user_id=1, proficiency=1, delta = 90)
