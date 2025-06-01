4a0812c57a20c7fa47ed6f9e0a530923f7b18841
Wood <jhuangchenleo@gmail.com>
2025-06-02 02:17:12 +0800

    fix: 手牌有空無法做hover動畫

--- modules/object/hand.py
+++ modules/object/hand.py
@@ -95,6 +95,7 @@ class Hand():
         '''
         使手牌可互動
         '''
+        self.__hovered_card_id = -1
         for card in self.__cards:
             if card != None:
                 card.can_press = True
@@ -123,6 +124,7 @@ class Hand():
                     continue
                 # 沒其它上層卡被hover就hover這張，一次只會有一張卡被 hover
                 if self.__hovered_card_id < i and card.is_hover: 
+                    print('hover動畫')
                     self.__hovered_card_id = i
                     x, y = self.__cards_pos[i]
                     card.moveTo((x, y - 100), 100, False)
