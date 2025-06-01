3f986bb832594a5321b18389135f872beeb4b406
RavenJiaSin <99117abraham@gmail.com>
2025-06-01 18:47:07 +0800

    dbUpdate: users.db 新增table:daily_cards feat: userDBconnect.py 新增功能 get_daily_cards() add_daily_cards() clear_daily_cards()

--- udbc_sample.py
+++ udbc_sample.py
@@ -15,7 +15,7 @@ user_db = UserDB()
 # user_db.add_card_to_user(user_id = 1, voc_id='2_adult')
 
 # 查詢使用者有的卡牌資訊
-print(user_db.get_card_info(user_id = 1))
+# print(user_db.get_card_info(user_id = 1))
 
 # 修改user的卡牌資料
 # user_db.update_card_info(user_id = 1, voc_id = '0_able', durability = 90)
@@ -25,3 +25,11 @@ print(user_db.get_card_info(user_id = 1))
 # print(user_db.get_card_durability_below(user_id = 1, durability = 100))
 
 # user_db.add_durability_for_proficiency(user_id=1, proficiency=1, delta = 90)
+
+# 新增每日卡牌
+user_db.add_daily_cards(user_id=1,voc_id='0_able')
+user_db.add_daily_cards(user_id=1,voc_id='1_above')
+# 查詢每日卡牌
+print(user_db.get_daily_cards(user_id = 1))
+#清空每日卡牌
+user_db.clear_daily_cards(user_id=1)
